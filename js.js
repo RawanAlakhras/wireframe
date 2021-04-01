44a78e96955e60afb462dd4bd761694517d3a6d6
Rawan.alakhras <ralakhras36@gmail.com>
2021-04-01 14:53:45 +0300

    change nav color based on user input

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 var num = prompt("plz enter number :")
+
 document.getElementById("num").innerHTML=num;
 if (num %2 ==0){
     alert("your number is even");
@@ -9,6 +10,8 @@ else{
     document.getElementById("num").innerHTML +=" and it is odd number.";
 }
 var user_name=prompt("plz enter your name:");
+var colors = prompt("plz enter your favorite color");
+document.getElementById("nav").style.backgroundColor = colors;
 if(user_name != "")
 {
     document.getElementById('name').innerHTML = user_name;
